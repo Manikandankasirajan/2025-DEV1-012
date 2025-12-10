a8f65300b7c99bcf09a95a5f3b862ea5b46fb706
Manikandankasirajan <itsmanikandankasirajan@gmail.com>
2025-12-10 15:01:43 +0530

    refact : tests to reduce repetition

--- DevelopmentBooks/src/tests/App.test.jsx
+++ DevelopmentBooks/src/tests/App.test.jsx
@@ -1,16 +1,15 @@
 import { render, screen } from "@testing-library/react";
 import App from "../App";
-import { expect } from "vitest";
+import { beforeEach, expect } from "vitest";
 import { testConstants } from "./constants/testConstants";
 
 describe("app component", () => {
+	beforeEach(() => render(<App />));
 	it("should render header text", () => {
-		render(<App />);
 		const headerText = screen.getByRole("heading", { level: 1 });
 		expect(headerText).toHaveTextContent(testConstants.HEADER_TEXT);
 	});
 	it("should render booklist section heading", () => {
-		render(<App />);
 		const bookListheading = screen.getByRole("heading", { level: 2 });
 		expect(bookListheading).toHaveTextContent(testConstants.BOOKLIST_HEADING);
 	});
